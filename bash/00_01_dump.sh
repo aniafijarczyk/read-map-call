ad5bfc955c434d014c60610fc3c68f307f255a29
aniafijarczyk <aniafijarczyk@gmail.com>
2023-09-07 15:14:56 -0400

    fastq extension

--- bash/00_01_dump.sh
+++ bash/00_01_dump.sh
@@ -7,4 +7,4 @@ SRA=${2}
 ### Checking size ( 7x for the output and 6x for the temp files)
 vdb-dump --info ${SRA} > ./00_sra/size_${SAMPLE}_${SRA}.txt
 grep 'size' ./00_sra/size_${SAMPLE}_${SRA}.txt
-fasterq-dump --split-3 -t ./ -o ${SAMPLE}_${SRA} -O 00_sra -e 6 -f ${SRA}
+prefetch ${SRA} & fasterq-dump --split-3 -t ./ -o ${SAMPLE}_${SRA}.fastq -O 00_sra -e 6 -f ${SRA}
