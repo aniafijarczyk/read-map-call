0811d7298b41edce5ac25cde573b502b9d9da769
aniafijarczyk <aniafijarczyk@gmail.com>
2024-08-12 16:36:47 -0400

    Update 08_01_bcftools.sh

--- bash/08_01_bcftools.sh
+++ bash/08_01_bcftools.sh
@@ -7,19 +7,19 @@ if [ -f "./06_rmdup/${SAMPLE}_rmdup.bam" ]; then
 
   if [ $PLOIDY = "1" ] ; then
   echo "Ploidy is 1"
-  bcftools mpileup -C50 -f ${REF} -min-MQ 4 -min-BQ 13 --skip-any-set 1796 -a FORMAT/AD,FORMAT/ADR,FORMAT/ADF,FORMAT/DP -Ou ./06_rmdup/${SAMPLE}_rmdup.bam | \
+  bcftools mpileup -C50 -f ${REF} -q4 -Q 13 --skip-any-set 1796 -a FORMAT/AD,FORMAT/ADR,FORMAT/ADF,FORMAT/DP -Ou ./06_rmdup/${SAMPLE}_rmdup.bam | \
   bcftools call -m -f gq -ploidy 1 -Oz -o ./08_bcftools/${SAMPLE}_bcftools.vcf.gz -
   tabix -f -p vcf ./08_bcftools/${SAMPLE}_bcftools.vcf.gz
 
   elif [ $PLOIDY = "2" ] ; then
   echo "Ploidy is 2"
-  bcftools mpileup -C50 -f ${REF} -min-MQ 4 -min-BQ 13 --skip-any-set 1796 -a FORMAT/AD,FORMAT/ADR,FORMAT/ADF,FORMAT/DP -Ou ./06_rmdup/${SAMPLE}_rmdup.bam | \
+  bcftools mpileup -C50 -f ${REF} -q4 -Q 13 --skip-any-set 1796 -a FORMAT/AD,FORMAT/ADR,FORMAT/ADF,FORMAT/DP -Ou ./06_rmdup/${SAMPLE}_rmdup.bam | \
   bcftools call -m -f gq -Oz -o ./08_bcftools/${SAMPLE}_bcftools.vcf.gz -
   tabix -f -p vcf ./08_bcftools/${SAMPLE}_bcftools.vcf.gz
 
   else
   echo "Ploidy is more than 2 and is set to 2"
-  bcftools mpileup -C50 -f ${REF} -min-MQ 4 -min-BQ 13 --skip-any-set 1796 -a FORMAT/AD,FORMAT/ADR,FORMAT/ADF,FORMAT/DP -Ou ./06_rmdup/${SAMPLE}_rmdup.bam | \
+  bcftools mpileup -C50 -f ${REF} -q4 -Q 13 --skip-any-set 1796 -a FORMAT/AD,FORMAT/ADR,FORMAT/ADF,FORMAT/DP -Ou ./06_rmdup/${SAMPLE}_rmdup.bam | \
   bcftools call -m -f gq -Oz -o ./08_bcftools/${SAMPLE}_bcftools.vcf.gz -
   tabix -f -p vcf ./08_bcftools/${SAMPLE}_bcftools.vcf.gz
   fi
